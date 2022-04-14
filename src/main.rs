d94eeb6f45ec3d75f227007e3644efc2e58eebb4
Redson <redson@riseup.net>
2022-04-14 08:14:09 -0300

    Uptime

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,38 @@ fn main() {
     let os = sys.name().unwrap();
     let kernel = sys.kernel_version().unwrap();
     let shell = env::var("SHELL").unwrap();
-    let random = rand::thread_rng().gen_range(1..=2);
+    let random: u32 = rand::thread_rng().gen_range(1..=2);
     let user = env::var("USER").unwrap().color(Color::DeepPink1a);
     let hostname = sys.host_name().unwrap().color(Color::Blue).bold();
     let mut charcount = env::var("USER").unwrap().chars().count();
+    charcount += sys.host_name().unwrap().chars().count();
     let mut line = "─".to_owned();
     let wm = Command::new("sh")
         .arg("-c")
         .arg("wmctrl -m | awk ' /Name/ {print $2}'")
         .output()
         .expect("wmctrl not installed.");
+    let uptime = Command::new("sh")
+        .arg("-c")
+        .arg("uptime -p | sed -n '1p' | sed 's/up //'")
+        .output()
+        .expect("lol");
+    let mut i: usize = 0;
 
-    let mut i = 0;
-    charcount += sys.host_name().unwrap().chars().count();
     while i < charcount {
         line += "─";
         i += 1;
-    }
+    } // increases the line size until it equals the number of characters of user + hostname
 
     if random < 2 {
         println!(
             "             {}@{}
    (\\_/)     {}
  __(. .)__   {} {}
- \\__|_|__/   {} {} 
-    / \\      {} {} 
+ \\__|_|__/   {} {}
+    / \\      {} {}             {} {}
              {} {}
- Talking is {}, show me the {}",
+ Talking is {}, show me the {}", // Sorry for this workaround, idk a better way to do that for now.
             user,
             hostname,
             line,
@@ -45,6 +50,8 @@ fn main() {
             os,
             "kernel:".color(Color::Blue).bold(),
             kernel,
+            "uptime:".color(Color::DeepPink1a),
+            String::from_utf8_lossy(&uptime.stdout),
             "shell:".color(Color::Yellow).bold(),
             shell,
             "wm:".color(Color::Green).bold(),
@@ -58,7 +65,7 @@ fn main() {
   __   __    {}
  (  \\,/  )   {} {}
   \\_ | _/    {} {}
-  (_/ \\_)    {} {}
+  (_/ \\_)    {} {}             {} {}
              {} {}
  Talking is {}, show me the {}",
             user,
@@ -68,6 +75,8 @@ fn main() {
             os,
             "kernel:".color(Color::Blue).bold(),
             kernel,
+            "uptime:".color(Color::DeepPink1a),
+            String::from_utf8_lossy(&uptime.stdout),
             "shell:".color(Color::Yellow).bold(),
             shell,
             "wm:".color(Color::Green).bold(),
